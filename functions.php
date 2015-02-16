85e1f067cf6cdc1c3b74ff923c1f7c0ec9670b64
misterair <mister.air@gmail.com>
2015-02-16 20:42:16 +0100

    ajout page image.php + plugin recent_article + etc

--- functions.php
+++ functions.php
@@ -271,24 +271,15 @@ function day_body_class($classes = '') {
   }
 }
 
-/*Widget similar articles*/
+/*WIDGET SIMILAR ARTICLES*/
 class similar_widget extends WP_Widget {
-
 function __construct() {
 parent::__construct(
-// Base ID of your widget
 'similar_widget', 
-
-// Widget name will appear in UI
 __('Similar articles Widget', 'similar_widget_domain'), 
-
-// Widget description
 array( 'description' => __( 'Show 3 similar articles with thumbnails', 'similar_widget_domain' ), ) 
 );
 }
-
-// Creating widget front-end
-// This is where the action happens
 public function widget( $args, $instance ) {
 $title = apply_filters( 'widget_title', $instance['title'] );
 echo $args['before_widget'];
@@ -328,8 +319,6 @@ echo $args['before_title'] . $title . $args['after_title'];
 </div>
 <?php
 }
-		
-// Widget Backend 
 public function form( $instance ) {
 if ( isset( $instance[ 'title' ] ) ) {
 $title = $instance[ 'title' ];
@@ -345,19 +334,76 @@ $title = __( 'New title', 'similar_widget_domain' );
 </p>
 <?php 
 }
-	
 // Updating widget replacing old instances with new
 public function update( $new_instance, $old_instance ) {
 $instance = array();
 $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 return $instance;
 }
-} // Class similar_widget ends here
-
+}
 // Register and load the widget
 function similar_load_widget() {
 	register_widget( 'similar_widget' );
 }
 add_action( 'widgets_init', 'similar_load_widget' );
- 
+
+
+/*WIDGET RECENT ARTICLE*/
+class recent_widget extends WP_Widget {
+function __construct() {
+parent::__construct(
+'recent_widget', 
+__('Recent article Widget', 'recent_widget_domain'), 
+array( 'description' => __( 'Show the last article with thumbnail', 'recent_widget_domain' ), ) 
+);
+}
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+?>
+     <ul class="relatedPosts">
+    <?php $the_query = new WP_Query( 'showposts=1' ); ?>
+
+    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+		<li class="relatedThumb">
+			<div class="roundIMG">
+				<?php the_post_thumbnail('archives-post-thumbnail',array(200,200));?>
+				<a rel="external" href="<? the_permalink()?>"><?php the_title(); ?></a>
+			</div>									
+		</li>
+    <?php endwhile;?>
+    </ul>
+</div>
+<?php
+}
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'recent_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+}
+// Register and load the widget
+function recent_load_widget() {
+	register_widget( 'recent_widget' );
+}
+add_action( 'widgets_init', 'recent_load_widget' );
+
 ?>
--- sidebar.php
+++ sidebar.php
@@ -5,20 +5,19 @@
 	<aside>
 	<div id="panel_contener">
 		<header>
-				<?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-					<div id="logo" class="picture">
-        				<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'>
-        					<img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
-        				</a>
-        			</div>
-					<?php else : ?>
-					<div id="logo" class="name">
-      				  <h1 class='site-title'>
-      				  	<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
-      				  </h1>
-      				</div>
-					<?php endif; ?>
-			</button>
+			<?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
+			<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'>
+				<div id="logo" class="picture">
+        			<img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+				</div>
+			</a>
+			<?php else : ?>
+			<div id="logo" class="name">
+      		  <h1 class='site-title'>
+      		  	<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
+      		  </h1>
+      		</div>
+			<?php endif; ?>
 		</header>
 		
 			<div id="panel_1">
