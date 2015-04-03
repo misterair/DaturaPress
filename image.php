3a98af8b9927bd3e45f07c44f58d7db63d7d0e9a
misterair <mister.air@gmail.com>
2015-04-03 15:30:15 +0200

    fix #5

---
+++ image.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<?php get_sidebar( 'article'); ?>
+
+<article>
+<?php if(have_posts()) : ?>
+<?php while(have_posts()) : the_post(); ?>
+<div id="lightbox" class="images">
+	<section class="largeThumbnail">
+		<div class="gradientPic top"></div>
+		<div class="ThumbBack">
+			<?php echo wp_get_attachment_image( get_the_ID(), full );?>
+		</div>
+		<?php echo wp_get_attachment_image( get_the_ID(), full);?>
+		<div class="gradientPic bottom"></div>
+	</section>
+</div>
+    
+<?php endwhile; ?>
+<footer class="read">
+	<div class="prev"><?php previous_image_link( false, '' ); ?></div>
+    
+	<a class="readAction return" href="<?php echo esc_url( get_permalink( $post->post_parent ) ) ?>"
+    title="<?php echo esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ) ?>" ></a>
+
+     <div class="next"><?php next_image_link( false, '' ); ?></div>
+
+</footer>
+<?php endif; ?>
+</article>
+<?php get_footer(); ?>
+
+	
